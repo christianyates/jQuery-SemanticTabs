7634293a5378b0af3b37aa56232395529756ad49
chrisyates <chris@christianyates.com>
2011-04-15 16:30:24 -0400

    Added GPL license information
    Signed-off-by: chrisyates <chris@christianyates.com>

--- jquery.semantictabs.js
+++ jquery.semantictabs.js
@@ -5,6 +5,21 @@ Chris Yates
 
 Inspired by Niall Doherty's jQuery Coda-Slider v1.1 - http://www.ndoherty.com/coda-slider
 
+Copyright (C) 2007-2011 Chris Yates
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 Usage:
 $("#mycontainer").semantictabs({
   panel:'.mypanelclass',         //-- Selector of individual panel body
